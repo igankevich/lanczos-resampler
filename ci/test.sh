41fb314e5510ff7f78b30688a67012a26b66bafd
Ivan Gankevich <ivan@igankevich.com>
2025-12-07 15:36:13 +0100

    CI

--- ci/test.sh
+++ ci/test.sh
@@ -3,10 +3,13 @@
 main() {
     set -ex
     os="$1"
+    workdir="$(mktemp -d)"
+    trap cleanup EXIT
     cargo_clippy
     cargo_test
     cargo_bench
     wasm_pack_build
+    add_browsers_to_path
     wams_pack_test
     wasm_integration_tests
 }
@@ -28,23 +31,34 @@ wasm_pack_build() {
     du -chd0 pkg
 }
 
-do_wasm_pack_test() {
-    wasm-pack test --headless --node --firefox --chrome "$@"
+add_browsers_to_path() {
+    mkdir "$workdir"/browsers
+    ln -s "$CHROME_PATH" "$workdir"/browsers/chrome
+    ln -s "$CHROMEDRIVER_PATH" "$workdir"/browsers/chromedriver
+    ln -s "$FIREFOX_PATH" "$workdir"/browsers/firefox
+    export PATH="$workdir"/browsers:"$PATH"
 }
 
 wasm_pack_test() {
     case "$os" in
-    linux | windows) do_wasm_pack_test ;;
-    macos) do_wasm_pack_test --safari ;;
+    macos) safari=" --safari" ;;
+    linux) ;;
     *)
         printf "Unknown os \"%s\"\n" "$os" >&2
         exit 1
         ;;
     esac
+    env WASM_BINDGEN_USE_BROWSER=1 \
+        wasm-pack test --headless --node --firefox --chrome"$safari"
+    wasm-pack test --node
 }
 
 wasm_integration_tests() {
     node tests.js
+}
+
+cleanup() {
+    rm -rf "$workdir"
 }
 
 main "$@"
