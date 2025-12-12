24fd506bec30742164efa313c21f14e8579a5725
Ivan Gankevich <ivan@igankevich.com>
2025-12-12 02:08:17 +0100

    CI

--- ci/gh-pages.sh
+++ ci/gh-pages.sh
@@ -18,7 +18,7 @@ generate_documentation() {
 
 push_to_gh_pages() {
     mkdir "$workdir"/gh-pages
-    git clone https://github.com/"$GITHUB_REPOSITORY" "$workdir"/gh-pages
+    git clone https://"$GITHUB_ACTOR":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY" "$workdir"/gh-pages
     git -C "$workdir"/gh-pages checkout gh-pages
     rsync -av --exclude .git --delete "$workdir"/docs/ "$workdir"/gh-pages/
     cd "$workdir"/gh-pages
